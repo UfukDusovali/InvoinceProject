1d4a651e7eb3809942448461725360e822d0848c
ufukdusovali <ufukdusovali@hotmail.com>
2022-02-21 16:34:35 +0300

    Add project files.

---
+++ InvoinceProject/Models/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoinceProject.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Product Code")]
+        public string ProductCode { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Product Qty")]
+        public int Quantity { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ InvoinceProject/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoinceProject.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please Enter User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        public string Password { get; set; }
+    }
+}
